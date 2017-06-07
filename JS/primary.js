1c5e8717538e9c7c1ee6fab7cb83a0f5cc44f917
Matt Tilley <mtilley255@gmail.com>
2017-06-07 11:59:40 -0500

    First commit

---
+++ JS/primary.js
@@ -0,0 +1,23 @@
+$(document).ready(function()
+{
+		$("[data-fancybox]").fancybox({
+		// Options will go here
+	});
+	
+});
+
+
+
+
+
+
+/*THIS IS FOR CYCLE2
+$("#modal_trigger_1").on("click", function()
+	{
+		$("#pic_swap_container").cycle({
+			fx:			"fade",
+			speed:		2000,
+			timeout:	3500
+			
+		});
+	});*/
